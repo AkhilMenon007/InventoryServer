ee9b22dec7789a4234e795e074b6fd3863593b12
Akhil Menon <menonakhilmenon@gmail.com>
2020-01-12 16:29:14 +0530

    Added a dummy inventory service

---
+++ InventoryService/InventoryService/Services/InventoryManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using InventoryService.Protos;
+using MongoDB.Bson;
+using InventorySystemMongoDB;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace InventoryService.Services
+{
+    public class InventoryManager:InventoryManagement.InventoryManagementBase
+    {
+        class Weapon: Item 
+        {
+            public int enchantment = 15;
+        }
+        class Armor : Item 
+        {
+            public int durability = 12;
+        }
+        [BsonDiscriminator(Required = true, RootClass = true)]
+        [BsonKnownTypes(typeof(Weapon),typeof(Armor))]
+        class Item 
+        {
+            [BsonId]
+            public Guid ID { get; set; }
+            public string Name { get; set; }
+            public int amount { get; set; }
+        }
+        public async override Task<ReplyMessage> DropItem(InventoryItem request, ServerCallContext context)
+        {
+            var mongoDBCommunicator = new MongoDBCommunicator("Inventory");
+            //var item = new Weapon { Name = "Sword", amount = 1 };
+            //await mongoDBCommunicator.InsertRecord("item", item);
+            //Console.WriteLine(item.ID.ToString());
+            var res = await mongoDBCommunicator.GetRecord<Item>("item", "be09cc7e-2796-45bc-8fe2-c25d0fb38dd8");
+
+            if (res is Weapon)
+            {
+                Console.WriteLine("The shit is a weapon");
+            }
+            if (res != null)
+            {
+                Console.WriteLine(res.ToJson());
+            }
+
+            return new ReplyMessage() { MessageCode = 0 };
+        }
+        public override Task<ItemObtainedMessage> GetItem(DroppedItem request, ServerCallContext context)
+        {
+            return base.GetItem(request, context);
+        }
+        public override Task<ItemObtainedMessage> TradeItem(InventoryItem request, ServerCallContext context)
+        {
+            return base.TradeItem(request, context);
+        }
+    }
+}
---
+++ InventoryService/InventorySystemMongoDB/MongoDBCommunicator.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventorySystemMongoDB
+{
+    public class MongoDBCommunicator
+    {
+        private IMongoDatabase db;
+
+        public MongoDBCommunicator(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        public MongoDBCommunicator(string database,string connectionString) 
+        {
+            var client = new MongoClient(connectionString);
+            db = client.GetDatabase(database);
+        }
+        public async Task InsertRecord<T>(string table,T record) 
+        {
+            var collection = db.GetCollection<T>(table);
+            await collection.InsertOneAsync(record);
+        }
+
+        public async Task<T> GetRecord<T>(string table,string id) 
+        {
+            var collection = db.GetCollection<T>(table);
+            Guid guid = new Guid(id);
+
+            var filter = Builders<T>.Filter.Eq("_id", guid);
+
+
+            return (await collection.FindAsync(filter)).FirstOrDefault();
+        }
+
+    }
+}
